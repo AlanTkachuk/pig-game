1f3e1a74454b4d964932a079b2e0be7238c83b62
Alan Navai <alannavai99@gmail.com>
2021-12-26 22:51:24 -0800

    Refactor reset function in init method

--- script.js
+++ script.js
@@ -12,13 +12,32 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let gameActive = true;
+let scores, gameActive, activePlayer, currentScore;
+
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+
+  // Reset scores
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+
+  // Reset styles
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+
+  // Start game
+  gameActive = true;
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -76,23 +95,4 @@ btnHold.addEventListener("click", function () {
 });
 
 // Reset event
-btnNew.addEventListener("click", function () {
-  // Reset scores
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  currentScore0El.textContent = 0;
-  currentScore1El.textContent = 0;
-  scores[0] = 0;
-  scores[1] = 0;
-  activePlayer = 0;
-  currentScore = 0;
-
-  // Reset styles
-  diceEl.classList.add("hidden");
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
-
-  gameActive = true;
-});
+btnNew.addEventListener("click", init);
